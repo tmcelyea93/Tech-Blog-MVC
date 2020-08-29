81a97198b374868c6217840e65da17d15f18b559
tmcelyea93 <tarahmcelyea@gmail.com>
2020-08-28 23:04:03 -0500

    added more files, started server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+// const path = require('path');
+// const exphbs = require('express-handlebars');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(session(sess));
+app.use(routes);
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening to PORT ${PORT}`));
+}); 
